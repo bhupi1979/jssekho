62069d24299db32411a0976b6b81f1aaec42c746
bhupi1979 <bhupendrapaliwal1983@gmail.com>
2023-12-31 16:55:31 +0530

    this is jssekho

---
+++ Main.js
@@ -0,0 +1,1 @@
+console.log("this ismy another file")
---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('bhupendrapaliwal');
+let a="23",b=24,c=undefined
+console.warn('ths sum of ab',parseFloat(a)+b.toString())
+console.log(typeof(parseFloat(a)))
+console.log(typeof(c))
+//***********stack and heap */
+//strings
+console.log(Number)
